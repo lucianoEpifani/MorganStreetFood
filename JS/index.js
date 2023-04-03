482c1b2d8c695301a56ec44744f2da439121c73e
Luciano <lucianocaffarone@gmail.com>
2023-04-03 20:57:27 +0200

    coomitongo

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector("#nav")
+
+const abrir = document.querySelector("#abrir")
+
+const cerrar = document.querySelector("#cerrar")
+
+const boton = document.querySelector("#btn")
+
+abrir.addEventListener('click', () =>{
+    nav.classList.add("visible")
+})
+
+cerrar.addEventListener('click', () =>{
+    nav.classList.remove("visible")
+})
+
+abrir.addEventListener('click', () =>{
+    btn.classList.add('invisible')
+})
+
+cerrar.addEventListener('click', () =>{
+    btn.classList.remove('invisible')
+})
+
